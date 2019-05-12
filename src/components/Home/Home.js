6f00ed73a762f22ca7ac9a263b46473dfe785e31
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-05-11 22:39:52 -0400

    Built components with JSX

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+class Home extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default Home;
