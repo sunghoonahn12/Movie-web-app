8c024ee5023f6657efa7dc27d153f020c1f60c29
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-05-12 15:59:07 -0400

    Scaffolding out the components for the home page

---
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './FourColGrid.css';
+
+const FourColGrid = (props) => {
+    return (
+        <div>
+            FourcolGrid
+        </div>
+    )
+}
+
+export default FourColGrid;
