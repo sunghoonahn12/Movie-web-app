fdd4e873a03f7263d0f8625044e2075cfbd3a3e3
Sunghoon Ahn <ahnsunghoon0215@gmail.com>
2019-05-13 00:54:44 -0400

    Used fetch to get API data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { API_URL, API_KEY, IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE} from '../../config';
 import HeroImage from '../elements/HeroImage/HeroImage';
 import SearchBar from '../elements/SearchBar/SearchBar';
 import FourColGrid from '../elements/FourColGrid/FourColGrid';
@@ -9,7 +10,28 @@ import './Home.css';
 
 class Home extends React.Component {
     state = {
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: ''
+    }
 
+    componentDidMount () {
+        this.setState({ loading: true});
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            this.setState({
+                moives: [...this.state.movies, ...result.results]
+            })
+        })
     }
 
     render() {
